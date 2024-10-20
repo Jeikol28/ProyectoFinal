96161beda5353caf2b7f922a112f6ffa35369857
Jeikol28 <jeikolalva@gmail.com>
2024-10-20 01:00:08 -0600

    ImplementacionPlataformas

--- js/app.js
+++ js/app.js
@@ -1,8 +1,16 @@
 
 var config = {
     type: Phaser.AUTO,
-    width: 800,
+    width: 900,
     height: 600,
+    physics:{
+        default: 'arcade',
+        arcade: {
+            gravity: {y:300},
+            debug: false
+        }
+    },
+    
     scene: {
         preload: preload,
         create: create,
@@ -14,15 +22,25 @@ var game = new Phaser.Game(config);
 
 function preload() {
  this.load.image('escenario','img/Prueba.png');
- this.load.image('plataforma','img/Plataformas.png')
+ this.load.image('plataforma','img/PlataformasJuego.png')
  this.load.image('estrella','img/estrellaJuego.png')
  this.load.image('enemigo','img/bolaJuego.png')
- this.load.image('personaje','img/personaje.png')
+ this.load.image('personaje','img/personajeJuego.png')
 }
 
 function create() {
-    this.add.image(425,300, 'escenario');
-    this.add.image(425,300, 'estrella');
+    this.add.image(450,300, 'escenario');
+
+    platforms = this.physics.add.staticGroup();
+
+
+    platforms.create(730, 360, 'plataforma');
+
+    platforms.create(170, 250, 'plataforma');
+
+    platforms.create(770, 150, 'plataforma');
+
+    
 }
 
 function update() {
