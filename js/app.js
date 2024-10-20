1079f0c41962820a36c69816665646523b643bcb
Jeikol28 <jeikolalva@gmail.com>
2024-10-20 13:00:12 -0600

    personaje

--- js/app.js
+++ js/app.js
@@ -23,6 +23,7 @@ var game = new Phaser.Game(config);
 function preload() {
  this.load.image('escenario','img/Prueba.png');
  this.load.image('plataforma','img/PlataformasJuego.png')
+ this.load.image('ground','img/ground.png')
  this.load.image('estrella','img/estrellaJuego.png')
  this.load.image('enemigo','img/bolaJuego.png')
  this.load.image('personaje','img/personajeJuego.png')
@@ -30,17 +31,26 @@ function preload() {
 
 function create() {
     this.add.image(450,300, 'escenario');
-
+    //Plataformas
     platforms = this.physics.add.staticGroup();
 
+    platforms.create(450, 650, 'ground').refreshBody();
 
-    platforms.create(730, 360, 'plataforma');
+    platforms.create(730, 400, 'plataforma');
+    platforms.create(170, 300, 'plataforma');
+    platforms.create(770, 200, 'plataforma');
 
-    platforms.create(170, 250, 'plataforma');
-
-    platforms.create(770, 150, 'plataforma');
-
+    //Jugador
+    player= this.physics.add.image(100, 450, 'personaje')
     
+    player.setCollideWorldBounds(true);
+    player.setBounce(0.2);// rebote cuando cae
+
+    player.body.setGravityY(300);//modificar gravedad solo personaje
+
+    this.physics.add.collider(player, platforms);//detecta la colicion
+
+   
 }
 
 function update() {
